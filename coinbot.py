1bd449b3e227c977446239841177d1e85c309f9e
Antônio Sales <antoniosales1003@gmail.com>
2023-03-24 13:18:35 -0300

    Corrigido endentamento das funções dentro da classe CoinBot.

--- coinbot.py
+++ coinbot.py
@@ -13,29 +13,29 @@ class CoinBot:
         },
     }
 
-#constante para a classe, tomando o nome do canal como parâmetro e setado como variável da instância
-def __init__(self, channel):
-    self.channel = channel
+    #constante para a classe, tomando o nome do canal como parâmetro e setado como variável da instância
+    def __init__(self, channel):
+        self.channel = channel
 
-#função para simular o flip da moeda e retornar uma mensagem
-def _flip_coin(self):
-    rand_int = random.randint(0,1)
-    if rand_int == 0:
-        results = "Cara"
-    else:
-        results = "Coroa"
+    #função para simular o flip da moeda e retornar uma mensagem
+    def _flip_coin(self):
+        rand_int = random.randint(0,1)
+        if rand_int == 0:
+            results = "Cara"
+        else:
+            results = "Coroa"
 
-    texto = f"O resultado é {results}"
+        texto = f"O resultado é {results}"
 
-    return {"type": "section", "text": {"type": "mrkdwn", "text": texto}},
+        return {"type": "section", "text": {"type": "mrkdwn", "text": texto}},
 
-# cria e retorna a mensagem
-def cria_mensagem(self):
-    return {
-        "channel": self.channel,
-        "blocks": [
-            self.COIN_BLOCK,
-            *self._flip_coin(),
-        ],
-    }
+    # cria e retorna a mensagem
+    def cria_mensagem(self):
+        return {
+            "channel": self.channel,
+            "blocks": [
+                self.COIN_BLOCK,
+                *self._flip_coin(),
+            ],
+        }
 
