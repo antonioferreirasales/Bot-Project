bd9c433dd18ba4f9c19f7931add0de9f7eb4f24c
Antônio Sales <antoniosales1003@gmail.com>
2023-03-24 12:35:26 -0300

    Adicionando função para simultar o flip e criando constante para a classe CoinBot

---
+++ coinbot.py
@@ -0,0 +1,30 @@
+#importa a biblioteca para gerar números aleatórios.
+import random
+
+class CoinBot: 
+    #constante com texto padrão
+    COIN_BLOCK = {
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": (
+                "Claro! Jogando a moeda \n\n"
+            ),
+        },
+    }
+
+#constante par aa clase, tomando o nome do canal como parâmetro e setado como variável da instância
+def __init__(self, channel):
+    self.channel = channel
+
+#função para simular o flip da moeda e retornar uma mensagem
+def _flip_coin(self):
+    rand_int = random.randint(0,1)
+    if rand_int == 0:
+        results = "Cara"
+    else:
+        results = "Coroa"
+
+    texto = f"O resultado é {results}"
+
+    return {"type": "section", "text": {"type": "mrkdwn", "text": texto}},
